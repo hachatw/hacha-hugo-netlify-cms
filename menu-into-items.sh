eb2d676d90e5804f5b42273cb2afe0c7ca5d9561
jyza11 <junyiza11@gmail.com>
2023-07-18 17:44:50 +0800

    Feat:new price hanlding shell

--- menu-into-items.sh
+++ menu-into-items.sh
@@ -31,7 +31,26 @@ do
     continue
   fi
 
+  # Remove all spaces before "NT" using sed
+  line="$(echo "$line" | sed -e 's/[[:space:]]*NT/NT/')"
+  
+  # Get the part after "NT"
+  part_after_nt="$(echo "$line" | grep -o '(?<=NT).*$')"
+
+  # Remove the part after "NT" from the original line
+  line="$(echo "$line" | sed -e 's/NT.*$//')"
+
   # Execute the "hugo new" command with the current line as input
   hugo new "$formated_folder/$line.md"
+
+  echo " this is $markdown_file at $formated_folder"
+
+  # Create temporary files for modification
+  temp_file=$(mktemp)
+  target_directory=$formated_folder
+  output_file="/content/zh/$target_directory/$line.md"
+
+  # Insert the part_after_nt content to the third line of the generated Markdown file
+  awk -v part="$part_after_nt" 'NR==3 {print part} {print}' "$output_file" > "$temp_file" && mv "$temp_file" "$output_file"
 
 done < "$markdown_file"
