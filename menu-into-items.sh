842576a361f849890625d8aa42f412c3307fc558
jyza11 <junyiza11@gmail.com>
2023-07-18 17:51:17 +0800

    Fix:start from relative dir

--- menu-into-items.sh
+++ menu-into-items.sh
@@ -48,7 +48,7 @@ do
   # Create temporary files for modification
   temp_file=$(mktemp)
   target_directory=$formated_folder
-  output_file="/content/zh/$target_directory/$line.md"
+  output_file="./content/zh/$target_directory/$line.md"
 
   # Insert the part_after_nt content to the third line of the generated Markdown file
   awk -v part="$part_after_nt" 'NR==3 {print part} {print}' "$output_file" > "$temp_file" && mv "$temp_file" "$output_file"
