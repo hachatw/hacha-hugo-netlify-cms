768d86ce0c020039fe1803b4df5184cfab85c914
jyza11 <junyiza11@gmail.com>
2023-08-09 05:34:07 +0800

    Refactor:shell genrated update data contents
    still testing and modifying new architypes

--- menu-into-items.sh
+++ menu-into-items.sh
@@ -35,26 +35,36 @@ do
   line="$(echo "$line" | sed -e 's/[[:space:]]*NT/NT/')"
   
   # Get the part after "NT" using awk (compatible with macOS)
-  part_after_nt=$(echo "$line" | awk -F 'NT' '{gsub(/^[[:space:]]+|[[:space:]]+$/, "", $2); print $2}')
+  price_mark=$(echo "$line" | awk -F 'NT' '{gsub(/^[[:space:]]+|[[:space:]]+$/, "", $2); print $2}')
 
   # Remove the part after "NT" from the original line
   line="$(echo "$line" | sed -e 's/NT.*$//')"
 
-  # Execute the "hugo new" command with the current line as input
-  hugo new "$formated_folder/$line.md"
-
-  echo " this is $markdown_file at $formated_folder"
 
   # Create temporary files for modification
   temp_file=$(mktemp)
   target_directory=$formated_folder
   output_file="./content/zh/$target_directory/$line.md"
 
-  # Format the part_after_nt content as "price: [part_after_nt]"
-  formatted_content="price: [$part_after_nt]"
+  # Execute the "hugo new" command with the current line as input
+  if [[ -f $output_file ]];then
+  echo "this is $line updating"
+  else
+  hugo new "$formated_folder/$line.md"
+  echo " create $markdown_file at $formated_folder"
+  fi
 
+  # Check if "price:[]" exists in the file
+  if grep -q 'price:' "$output_file"; then
+  # Replace "price:[]" with "price: "price_mark""
+  echo "there is price tags"
+  sed -e "s/^price:.*/price: [$price_mark] /" "$output_file" > "$temp_file" && mv "$temp_file" "$output_file"
+  else
+  echo "no price create one "
+  # Format the part_after_nt content as "price: [part_after_nt]"
+  formatted_content="price: [$price_mark] "
   # Insert the formatted content to the third line of the generated Markdown file
   awk -v content="$formatted_content" 'NR==5 {print content} 1' "$output_file" > "$temp_file" && mv "$temp_file" "$output_file"
-
+  fi
 
 done < "$markdown_file"
