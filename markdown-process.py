ee38cdcabdc022074bf9e5fbfd97e0b859a35423
jyza11 <junyiza11@gmail.com>
2023-08-24 11:50:44 +0800

    Test:Categpries Replace function

--- markdown-process.py
+++ markdown-process.py
@@ -6,6 +6,32 @@ def read_markdown_file(filename):
         for line in file:
             lines.append(line.strip())
     return lines
+    
+def replace_section_with_list(directory, file_name, section_name, new_values):
+    file_path = f"{directory}/{file_name}"
+    
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    section_start = content.find(section_name)
+    if section_start == -1:
+        print(f"Section '{section_name}' not found in the file.")
+        return
+
+    section_end = content.find('\n', section_start)
+    if section_end == -1:
+        section_end = len(content)
+
+    new_content = f"{section_name}\n"
+    for value in new_values:
+        new_content += f"  - {value}\n"
+
+    updated_content = content[:section_start] + new_content + content[section_end:]
+
+    with open(file_path, 'w') as file:
+        file.write(updated_content)
+
+# Replace the "categories" section with values from a list
 
 def main():
     if len(sys.argv) != 2:
@@ -17,10 +43,16 @@ def main():
 
     for line in lines:
         if line.startswith("title"):
-            print(line)  # Only print lines that start with #
+            categories = line 
+            directory = 'content/zh/items/' + categories
+            print(directory)  # Only print lines that start with #
         if line.startswith("#"):
-            print(line)  # Only print lines that start with #
+            categories_append = line 
+            print(categories_append)  # Only print lines that start with #
         if line.startswith("*"):
-            print(line)  # Only print lines that start with *
+            items_file_name = line
+            new_values = ['categories', 'categories_append']
+            replace_section_with_list(directory, items_file_name, 'categories:', new_values)
+            print(new_values)  # Only print lines that start with *
 if __name__ == '__main__':
     main()
