41810f58fd92fec713aedc6cd738b1adbb82f12a
jyza11 <junyiza11@gmail.com>
2023-08-31 03:31:37 +0800

    Feat:support - markdown note and refactor

--- markdown-process.py
+++ markdown-process.py
@@ -9,6 +9,36 @@ def read_markdown_file(filename):
             lines.append(line.strip())
     return lines
     
+def process_line(line):
+    # Remove any number of white spaces after asterisk and dash 
+    if line.startswith("-"):
+        line = re.sub(r'\-\s+', '-', line)
+        line = line.replace('-', '')
+
+    if line.startswith("*"):
+        line = re.sub(r'\*\s+', '*', line)
+        line = line.replace('*', '')
+
+    # Remove white space before "NT" but keep after non-space characters
+    line = re.sub(r'(?<=[^\s])\s+NT', ' NT', line)
+    line = line.split(' NT')[0] 
+    # Remove text after NT
+    line = line.split(' NT')[0]
+
+    # Remove any number of hash symbols followed by space
+    line = re.sub(r'#+\s', '', line)
+
+    # Remove "title:" and keep the data
+    if line.startswith('category:'):
+        line = line.replace('category:', '').strip()
+
+    if line.startswith('categories:'):
+        line = line.replace('categories:', '').strip()
+
+    # Remove white spaces at the beginning and end of the line
+    line = line.strip()
+    return line
+
 def replace_section_with_list(directory, file_name, section_name, new_values):
     file_path = f"{directory}/{file_name}.md"
     
@@ -38,27 +68,6 @@ def replace_section_with_list(directory, file_name, section_name, new_values):
 
 # Replace the "categories" section with values from a list
 
-def process_line(line):
-    # Remove leading and trailing whitespaces
-    line = line.strip()
-
-    # Remove asterisks * and hash # symbols
-    line = line.replace('* ', '')
-    # Remove any number of hash symbols followed by space
-    line = re.sub(r'#+\s', '', line)
-    # Remove text after NT
-    line = line.split(' NT')[0]
-
-    # Remove "title:" and keep the data
-    if line.startswith('category:'):
-        line = line.replace('category:', '').strip()
-
-    if line.startswith('categories:'):
-        line = line.replace('categories:', '').strip()
-
-    return line
-
-
 def main():
     if len(sys.argv) != 2:
         print("Usage: python script.py <markdown_file>")
@@ -66,24 +75,37 @@ def main():
 
     markdown_file = sys.argv[1]
     lines = read_markdown_file(markdown_file)
-    list_category=[]
-
     main_category=""
-
+    categories_append=""
+    list_category=[]
+    
     for line in lines:
         if line.startswith("category:"):
             main_category = process_line(line) 
 
         directory = 'content/zh/items/' + main_category
-        print (directory + line)  # Only print lines that start with #
+        print (directory + "this is " + line)  # Only print lines that start with #
 
         if line.startswith("#"):
             categories_append = process_line(line) 
             list_category.append(categories_append)
-            print("\n" +categories_append+"\n")
+            print("\n Append" +categories_append+"\n")
             print( directory + "/" + categories_append)
             command = ['hugo', 'new', f'{directory}/{categories_append}/_index.md']
             subprocess.run(command, capture_output=True, text=True)
+
+        if line.startswith("-"):
+            try:
+                items_file_name = process_line(line)
+                new_values = [main_category, categories_append]
+                replace_section_with_list(directory, items_file_name, 'categories:', new_values)
+                command = ['mv', f'{directory}/{items_file_name}.md' , f'{directory}/{categories_append}/{items_file_name}.md']
+                subprocess.run(command, capture_output=True, text=True)
+                print(items_file_name)
+                print(new_values)  # Only print lines that start with *
+                
+            except FileNotFoundError:
+                print(items_file_name + "not found")
 
         if line.startswith("*"):
             try:
@@ -94,6 +116,7 @@ def main():
                 subprocess.run(command, capture_output=True, text=True)
                 print(items_file_name)
                 print(new_values)  # Only print lines that start with *
+
             except FileNotFoundError:
                 print(items_file_name + "not found")
     print(list_category)
